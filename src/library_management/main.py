87082723d9d92642fcb8177c2cce77f0afda06fe
JSaidi1 <jabrane_saidi@outlook.com>
2025-12-29 19:39:23 +0100

    feat: implement consult action

--- src/library_management/main.py
+++ src/library_management/main.py
@@ -1,6 +1,24 @@
+from data.documents_store_data import list_documents
+
+
 # =================================================================================================================
 #                                                   FUNCTIONS                            
 # =================================================================================================================
+def display_list(liste_documents) -> dict:
+    cpt = 1
+    for document in liste_documents:
+        print(cpt,". ", end="")
+        document.show_informations()
+        cpt += 1
+
+def choose_document(liste_documents):
+    display_list(liste_documents)
+
+    # Get indexes of docs in liste_documents
+    user_document_choice = int(input("\nPlease choose the document number (book/magazine): "))
+
+    return user_document_choice
+
 def display_menu():
     print("""
         ==========================================================================
@@ -15,29 +33,32 @@ def display_menu():
         """)
     
     while(1):
-        choosen_action = input("Choose action to execute (1 or 2 or ... or '0' to exit: ")
+        choosen_action = input("Choose action to execute (1 or 2 or ... or '0' to exit): ")
 
         if choosen_action == "0":
             print("\nYou chose to exit. Bye, see you!")
             break
         elif choosen_action == "1":
-            print("\nYou chose: '1. Consult'")
+            print("\nYou chose: '1. Consult': ")
             #Consult
+            document_id = choose_document(list_documents)
+            list_documents[document_id-1].consult()
+            print("\n")
             break
         elif choosen_action == "2":
-            print("\nYou chose: '2. Borrow'")
+            print("\nYou chose: '2. Borrow': ")
             #Borrow
             break
         elif choosen_action == "3":
-            print("\nYou chose: '3. Give back'")
+            print("\nYou chose: '3. Give back': ")
             #Give back
             break
         elif choosen_action == "4":
-            print("\nYou chose: 'Add Document (admin)'")
+            print("\nYou chose: 'Add Document (admin)': ")
             #Add Document
             break
         elif choosen_action == "5":
-            print("\nYou chose: 'Remove Document (admin)'")
+            print("\nYou chose: 'Remove Document (admin)': ")
             #Remove Document
             break
         else:
