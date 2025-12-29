4f15ec168387adfcf5b85a52659a846e021eee96
JSaidi1 <jabrane_saidi@outlook.com>
2025-12-29 17:36:43 +0100

    feat: create magazine class

---
+++ src/library_management/models/magazine.py
@@ -0,0 +1,14 @@
+from src.library_management.interfaces.consultable import Consultable
+from src.library_management.models.document import Document
+
+
+class Magazine(Document, Consultable):
+    def __init__(self, titre: str, year_of_publication: int, number: int):
+        super().__init__(titre, year_of_publication)
+        self.number = number
+
+    def show_informations(self):
+        print(f"Magazine ({self.title, self.year_of_publication, self.number})")
+
+    def consulter(self):
+        super().consult()
